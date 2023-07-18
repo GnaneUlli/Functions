c91624f4cb517d796d63e52c991d853d31d37636
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-07-18 15:12:18 +0530

    Create Mcq.c

---
+++ Mcq.c
@@ -0,0 +1,29 @@
+1.
+(i) Default return type of a function is int.
+(ii) Default return type of a function is void.
+(iii) Function can return structure.
+(iv) Function can return enum and its elements.
+(v) Function can not return enum.
+(vi) All types of variables, functions, and structures can become static
+in C.
+(a) i, iii, v
+(b) i, iv, v
+(c) i, iii, iv, vi
+(d) ii, iii, iv, vi
+
+Ans : C
+
+2.
+#include<stdio.h>
+enum block{a = 0 , b = -4 , enum subbloc{i , j = 3} sb,
+c}; // line1
+int main()
+{
+enum block bl.
+static register p ; //line 4
+p = a + bl.sb.i; //line 5
+printf("%d ",p);
+return 0;
+}
+
+Ans : Error at line1, line2, line3.
